4820c2ccb25ccacc9dc5582237a509ff7248e276
Jola2 <jolazborek@gmail.com>
2022-03-31 20:05:07 +0200

    Sending changes

--- exercise.py
+++ exercise.py
@@ -1,13 +1,4 @@
 
-def sum(a,b):
-  return a + b
-
-def multiply(a, b):
-  return a * b
-
-
-print(f'Sum: 3 + 3 = {sum(3,3)}')
-print(f'multiplication: 3 + 3 = {multiply(3,3)}')
 
 def sum(a, b):
     return a + b
